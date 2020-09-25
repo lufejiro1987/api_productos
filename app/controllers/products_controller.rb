befa9e7ee0f7a3c14b1901e41f2ad227636ebbff
Luis Felipe Jimenez Roldan <luisfelipejimenezroldan@gmail.com>
2020-09-25 17:09:46 -0300

    terminando

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,11 @@ class ProductsController < ApplicationController
 
         render json: @products
     end
+
+    def show
+        @category = Caegory.find_by(name: params[:name])
+        @products = Product.where(category_id: @category.id, id: params[:id])
+
+        render json: @products
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'categories', to: 'categories#index'
   get 'categories/:name', to: 'categories#show'
-  get 'categories/:name/:products', to: 'products#index'
+
+  get 'categories/:name/products', to: 'products#index'
+  get 'categories/:name/products/:id', to: 'products#show'
 end
